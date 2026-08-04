25c270c9028589d31e9229e29d841fe7bbf7e120
amkarima <65171426+amkarima@users.noreply.github.com>
2026-08-04 17:08:13 +0200

    Add files via upload

---
+++ public/localEnv.cjs
@@ -0,0 +1,8 @@
+    export const apiURLLocal = 'https://api.automatescore.app/v1/playwright'
+    export const APIKeyLocal= 'pk_a6acc9fbaea9371574c6df618e8ee4091963bc77b5fc62cc76ae0960ba63bf64'
+    export const secretKeyLocal = 'sk_e248b27e6e40066e244abbcf76193371d318a1c03f3d13c36b1f8fbeb631fb51'
+    export const testValueLocal= "testValueDataOrigin"
+    export const systemNameLocal = 'Sofinco Sanity'
+    export const campaignNameLocal = 'Sofinco Sanity'
+    
+    
